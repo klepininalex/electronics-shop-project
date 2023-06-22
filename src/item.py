859c6fd93081d1e7b39347f269accb93dc117b8c
klepininalex <klepininalex@gmail.com>
2023-06-22 15:42:49 +0300

    solution homework 4

--- src/item.py
+++ src/item.py
@@ -16,16 +16,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
 
-    def __repr__(self):
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
+
+    def __add__(self, other_instance) -> int:
+        if not issubclass(other_instance.__class__, self.__class__):
+            raise TypeError('Возможно сложить только с экземплярами Phone или Item классов')
+        return self.quantity + other_instance.quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -50,12 +55,12 @@ class Item:
         self.__name = name if len(name) <= 10 else 'Exception: Длина наименования товара превышает 10 символов.'
 
     @classmethod
-    def instantiate_from_csv(cls):
+    def instantiate_from_csv(cls) -> None:
         Item.all.clear()
         with open('../src/items.csv', 'r') as file:
             items = csv.DictReader(file)
             for row in items:
-                cls(row['name'], row['price'], row['quantity'])
+                cls(row[int('name')], float(row[int('price')]), int(row[int('quantity')]))
 
     @staticmethod
     def string_to_number(number: str) -> int:
